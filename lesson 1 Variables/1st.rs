545ac310904225eb847a7761b074d75a571d2b70
Danny <dnioko0412@gmail.com>
2023-09-30 21:08:05 +0800

    first lesson in 2023.09.30

---
+++ lesson 1 Variables/1st.rs
@@ -0,0 +1,82 @@
+// Fix the error below with least amount of modification to the code
+fn main() {
+    // let x: i32; // Uninitialized but used, ERROR !
+    // let _y: i32; // Uninitialized but also unused, only a Warning !
+
+    // assert_eq!(x, 5);
+    // println!("Success!");
+    
+    //   let mut x: i32 = 1;
+    // x += 2; 
+    
+    // assert_eq!(x, 3);
+    // println!("Success!");
+    
+    //     let x: i32 = 10;
+    //   let y: i32 = 5;
+    // {
+      
+    //     println!("The value of x is {} and value of y is {}", x, y);
+    // }
+    // println!("The value of x is {} and value of y is {}", x, y); 
+    
+//     fn main() {
+   
+//     define_x();
+   
+// }
+
+// fn define_x() {
+//     let x: &str = "hello";
+    
+//      println!("{}, world", x); 
+// }
+
+//   let x: i32 = 5;
+//     {
+//         let x = 12;
+//         assert_eq!(x, 12);
+//     }
+
+//     assert_eq!(x, 5);
+
+//     let x: i32 = 42;
+//     println!("{}", x); // Prints "42".
+
+//   let mut x: i32 = 1;
+//     x = 7;
+//     // Shadowing and re-binding
+//     let mut x = x; 
+    
+//     x += 3;
+
+//     let y: i32 = 4;
+//     // Shadowing
+//     let y: &str = "I can also be bound to text!"; 
+
+//     println!("Success!");
+
+// #[allow(unused_variables)]
+// fn main() {
+//     let x = 1; 
+// }
+
+// Warning: unused variable: `x`
+ 
+    //  let (mut x, y) = (1, 2);
+    // x += 2;
+
+    // assert_eq!(x, 3);
+    // assert_eq!(y, 2);
+
+    // println!("Success!");
+    
+    //     let (x, y);
+    // (x,..) = (3, 4);
+    // [.., y] = [1, 2];
+    // // Fill the blank to make the code work
+    // assert_eq!([x,y], [3, 2]);
+
+    // println!("Success!");
+    
+}
